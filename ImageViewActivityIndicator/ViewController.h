06f468e5078e5e46326e4026222fae07fe82a193
Sergey Yuzepovich <yuzepovich@gmail.com>
2014-12-30 15:52:07 +0300

    Initial Commit

---
+++ ImageViewActivityIndicator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageViewActivityIndicator
+//
+//  Created by Sergey Yuzepovich on 30.12.14.
+//  Copyright (c) 2014 Sergey Yuzepovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageViewActivityIndicator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ImageViewActivityIndicator
+//
+//  Created by Sergey Yuzepovich on 30.12.14.
+//  Copyright (c) 2014 Sergey Yuzepovich. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
